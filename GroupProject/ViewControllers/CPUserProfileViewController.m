79ed12abd79bd7c0e15df9aa724088bb9894e249
Eugenia <eleong@zynga.com>
2014-04-12 16:45:00 -0700

    Files for view controllers

---
+++ GroupProject/ViewControllers/CPUserProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  CPUserProfileViewController.m
+//  GroupProject
+//
+//  Created by Eugenia Leong on 4/12/14.
+//  Copyright (c) 2014 CodePath Group Project. All rights reserved.
+//
+
+#import "CPUserProfileViewController.h"
+
+@interface CPUserProfileViewController ()
+
+@end
+
+@implementation CPUserProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
