7670417021c3874f4f0288a1fc23bebc963d3954
seville <seville@yahoo-inc.com>
2014-04-13 13:35:59 -0700

    Added filtering of pins, still need to make them group properly

--- GroupProject/ViewControllers/CPViewLocationViewController.m
+++ GroupProject/ViewControllers/CPViewLocationViewController.m
@@ -13,8 +13,12 @@
 #import "CPRackAnnotation.h"
 #import "CPRackMiniDetailViewController.h"
 
+#define iphoneScaleFactorLatitude   9.0
+#define iphoneScaleFactorLongitude  11.0
+
 @interface CPViewLocationViewController ()
 @property (weak, nonatomic) IBOutlet MKMapView *mainMapView;
+@property (nonatomic, strong) NSMutableArray *annotations;
 
 
 @end
@@ -36,7 +40,8 @@
     [super viewDidLoad];
     
     self.mainMapView.delegate = self;
-   
+    self.annotations = [[NSMutableArray alloc] init];
+		
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,7 +57,7 @@
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:fileData options:kNilOptions error:nil];
     
      NSArray *data = [json objectForKey:@"data"];
-    
+    [self.annotations removeAllObjects];
    for (NSArray *row in data) {
        double latitude = [[[row objectAtIndex:22]objectAtIndex:1] doubleValue];
        double longitude = [[[row objectAtIndex:22]objectAtIndex:2] doubleValue];
@@ -70,8 +75,10 @@
        coordinate.longitude = longitude;
        
        CPRackAnnotation *annotation = [[CPRackAnnotation alloc] initWithTitle:rack.name Location:coordinate];
-       [self.mainMapView addAnnotation:annotation];
+       //[self.mainMapView addAnnotation:annotation];
+	   [self.annotations addObject:annotation];
 	}
+	[self.mainMapView addAnnotations:[self filterAnnotations:self.annotations modifyMap:false]];
 }
 
 #pragma mark - mapview delegate methods
@@ -88,9 +95,14 @@
 
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
     
-    MKPinAnnotationView *annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
-                                                     reuseIdentifier:@"RackAnnotation"];
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"RackAnnotation"];
+	
+	if(!annotationView){
+		annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"RackAnnotation"];
+	}
+	
     annotationView.pinColor = MKPinAnnotationColorRed;
+
     annotationView.canShowCallout = YES;
     annotationView.draggable = YES;
     
@@ -112,6 +124,45 @@
     } ];
     
     
+}
+
+-(void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated{
+	[self filterAnnotations:self.annotations modifyMap:true];
+}
+
+
+/* from: https://github.com/kviksilver/MKMapview-annotation-grouping/blob/master/mapView/mapViewViewController.m */
+-(NSArray *)filterAnnotations:(NSArray *)racksToFilter modifyMap:(BOOL)modifyMap{
+    float latDelta=self.mainMapView.region.span.latitudeDelta/iphoneScaleFactorLatitude;
+    float longDelta=self.mainMapView.region.span.longitudeDelta/iphoneScaleFactorLongitude;
+	
+    NSMutableArray *racksToShow=[[NSMutableArray alloc] initWithCapacity:0];
+	
+    for (int i=0; i<[racksToFilter count]; i++) {
+        CPRackAnnotation *checkingLocation=[racksToFilter objectAtIndex:i];
+        CLLocationDegrees latitude = checkingLocation.coordinate.latitude;
+        CLLocationDegrees longitude = checkingLocation.coordinate.longitude;
+		
+        bool found=FALSE;
+        for (CPRackAnnotation *tempPlacemark in racksToShow) {
+            if(fabs(tempPlacemark.coordinate.latitude-latitude) < latDelta &&
+               fabs(tempPlacemark.coordinate.longitude-longitude) <longDelta ){
+                found=TRUE;
+				if (modifyMap){
+					[self.mainMapView removeAnnotation:checkingLocation];
+				}
+                break;
+            }
+        }
+        if (!found) {
+            [racksToShow addObject:checkingLocation];
+            if (modifyMap){
+				[self.mainMapView addAnnotation:checkingLocation];
+			}
+        }
+		
+    }
+    return racksToShow;
 }
 
 
