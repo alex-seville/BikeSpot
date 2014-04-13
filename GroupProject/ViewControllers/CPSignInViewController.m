34ae62246a129e4869bf0ce6f2de08e41ca82ed8
Eugenia <eleong@zynga.com>
2014-04-12 18:45:55 -0700

    Sign in w/ FB

--- GroupProject/ViewControllers/CPSignInViewController.m
+++ GroupProject/ViewControllers/CPSignInViewController.m
@@ -8,6 +8,8 @@
 
 #import "CPSignInViewController.h"
 #import "CPEnterAccountViewController.h"
+#import <Parse/Parse.h>
+#import "CPViewLocationViewController.h"
 
 @interface CPSignInViewController ()
 - (IBAction)signInFB:(id)sender;
@@ -54,6 +56,30 @@
 }
 
 - (IBAction)signInFB:(id)sender {
+    // The permissions requested from the user
+    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+    
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        
+        if (!user) {
+            if (!error)
+            {
+                NSLog(@"Uh oh. The user cancelled the Facebook login.");
+            }
+            else
+            {
+                NSLog(@"Uh oh. An error occurred: %@", error);
+            }
+        }
+        else {
+            NSLog(@"User with facebook logged in!");
+            //[self.navigationController pushViewController:[[UserDetailsViewController alloc] initWithStyle:UITableViewStyleGrouped] animated:YES];
+            CPViewLocationViewController *viewLocationViewController = [[CPViewLocationViewController alloc] init];
+            UINavigationController * navigationController = [[UINavigationController alloc] initWithRootViewController:viewLocationViewController];
+            [self presentViewController:navigationController animated:YES completion:nil];
+        }
+    }];
 }
 
 - (IBAction)SignIn:(id)sender {
